7713075990d66f141ec65f145a6c50abed5c0bc9
Moin <sci.mointariq@gmail.com>
2025-08-28 17:48:45 +0800

    Update job.py

--- job.py
+++ job.py
@@ -18,7 +18,7 @@ TARGET_YEAR, TARGET_MONTH = YESTERDAY.year, YESTERDAY.month
 # ----------------------------
 # Load company list
 # ----------------------------
-list_comp = pd.read_excel("Companies_listed_PSX_Cleaned.xlsx").head()
+list_comp = pd.read_excel("Companies_listed_PSX_Cleaned.xlsx")
 
 # ----------------------------
 # Global session setup
